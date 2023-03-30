eea32fde32764a3a3c931e90751aff658cbd2250
Srijan <100436695+TruecoderSri@users.noreply.github.com>
2023-03-31 01:13:33 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: "us-east-1_gQDqnV4re", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "2f3usqps00sohkdv2fm8h4ojte", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: " https://4resg9t9v6.execute-api.us-east-1.amazonaws.com/prod1", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
+};
